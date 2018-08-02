f3527a1839fb361d829727f06d7f5777e22b1213
Henry Smith <henry@henrysmith.org>
2018-08-02 13:40:29 +0200

    Get slider working

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const input = document.querySelector('.input');
+  const slider = document.querySelector('.slider');
+  const output = document.querySelector('.output');
+
+  const originalWidth = output.clientWidth;
+
+  const copyInput = () => output.textContent = input.value;
+
+  const updateSize = () => {
+    const sizeMultiplier = slider.value / 100;
+    const newWidth = originalWidth * sizeMultiplier;
+    output.style.width = `${newWidth}px`;
+  };
+
+  input.addEventListener('keyup', copyInput);
+  slider.addEventListener('change', updateSize);
+
+  copyInput();
+
+  console.log('go!', input, slider, output);
+});
